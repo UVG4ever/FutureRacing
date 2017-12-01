b0c2dfdc9bff6030a20380a2c163bf801b1a9b73
Gus Gama <beattguss@gmail.com>
2017-11-30 21:58:10 -0600

    Pista de la donita

---
+++ FutureRacing/Assets/Scripts/camaraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class camaraMovement : MonoBehaviour
+{
+    public GameObject spartan;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position =    new Vector3(spartan.transform.position.x, spartan.transform.position.y+2.2f, spartan.transform.position.z- 10f);
+      
+
+
+
+    }
+}
+
